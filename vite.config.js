9dbc8a8ba1d9ff87433304feac482c4e89aa238c
Hua Wang <hwa205@uclive.ac.nz>
2025-07-13 09:02:18 +1200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 自定义域名使用根路径
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
